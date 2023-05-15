743048e148037cd5715bb2e20b703f6a31c81ff2
byulcode <bybit0408@gmail.com>
2023-05-15 18:27:23 +0900

    chore : swagger 설정

--- src/main/java/com/study/proudcat/domain/post/controller/PostController.java
+++ src/main/java/com/study/proudcat/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.study.proudcat.domain.post.controller;
 
 import com.study.proudcat.domain.post.dto.request.WritePostReqeust;
 import com.study.proudcat.domain.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "게시물 작성", description = "게시물 작성 메서드입니다.")
     @PostMapping
     public ResponseEntity<Void> writePost(@RequestBody WritePostReqeust request) {
         postService.writePost(request);
