4db57dfc6a606f3e0d6af91ef5e813b64c0d4df0
byulcode <bybit0408@gmail.com>
2023-05-23 23:01:37 +0900

    feat : 파일 업로드 기능 구현

---
+++ src/main/java/com/study/proudcat/domain/storage/controller/StorageController.java
@@ -0,0 +1,29 @@
+package com.study.proudcat.domain.storage.controller;
+
+import com.study.proudcat.domain.storage.service.StorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/proudcat/image")
+@RequiredArgsConstructor
+public class StorageController {
+
+    private final StorageService storageService;
+
+    @Operation(summary = "이미지 업로드", description = "multipart/form-data 형식의 이미지 리스트를 input으로 받습니다.")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = storageService.uploadImage(file);
+        return ResponseEntity.ok(uploadImage);
+    }
+}
