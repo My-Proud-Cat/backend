54c4c0c9e7e873c6d8f1aabf091305807032a915
byulcode <bybit0408@gmail.com>
2023-06-30 13:44:14 +0900

    refactor : api 주소 및 스웨거 설명 변경

--- src/main/java/com/study/proudcat/domain/storage/controller/StorageController.java
+++ src/main/java/com/study/proudcat/domain/storage/controller/StorageController.java
@@ -12,20 +12,20 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/proudcat/image-db-storage")
+@RequestMapping("/proudcat/api/image-db-storage")
 @RequiredArgsConstructor
 public class StorageController {
 
     private final StorageService storageService;
 
-    @Operation(summary = "이미지 업로드", description = "multipart/form-data 형식의 이미지 리스트를 input으로 받습니다.")
+    @Operation(summary = "이미지 업로드(db에 저장)", description = "업로드한 이미지 파일을 db에 저장합니다.")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
         String uploadImage = storageService.uploadImage(file);
         return ResponseEntity.ok(uploadImage);
     }
 
-    @Operation(summary = "이미지 다운로드", description = "이미지 fileNmae을 input으로 받습니다.")
+    @Operation(summary = "이미지 다운로드(db에서 다운)", description = "이미지 fileName을 파라미터로 받아 db에 저장된 이미지를 다운로드합니다.")
     @GetMapping("{fileName}")
     public ResponseEntity<?> downloadImage(@PathVariable(name = "fileName") String fileName) {
         byte[] downloadImage = storageService.downloadImage(fileName);
