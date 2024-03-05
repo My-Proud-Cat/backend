deaa8778f81b00ffaed0ee63e2ffce713edda61a
byulcode <bybit0408@gmail.com>
2024-03-05 16:10:46 +0900

    test: ci 스크립트 테스트

--- src/main/java/com/study/proudcat/infra/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/study/proudcat/infra/security/jwt/JwtAuthenticationFilter.java
@@ -24,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private static final String AUTHENTICATION_HEADER = "Authorization";
 	private final JwtTokenProvider jwtTokenProvider;
-
+test
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
